aecb37ab6f9f9aa15d48e0de2e3f94569d12535a
Marwin Misselhorn <marwinmisselhorn@hotmail.de>
2021-07-06 03:16:10 +0200

    Add raw frames option

--- src/events/game.ts
+++ src/events/game.ts
@@ -2,14 +2,17 @@ import type { Observable } from "rxjs";
 import { map, switchMap, withLatestFrom } from "rxjs/operators";
 
 import type { RxSlpStream } from "../stream";
-import type { GameEndPayload, GameStartType } from "../types";
+import { playerFrameFilter } from "../operators/frames";
 import { findWinner } from "../utils";
+import { GameStartType, GameEndPayload, FrameEntryType } from "../types";
+import { forAllPlayerIndices } from "../utils";
 
 export class GameEvents {
   private stream$: Observable<RxSlpStream>;
 
   public start$: Observable<GameStartType>;
   public end$: Observable<GameEndPayload>;
+  public rawFrames$: Observable<FrameEntryType>;
 
   public constructor(stream: Observable<RxSlpStream>) {
     this.stream$ = stream;
@@ -26,5 +29,12 @@ export class GameEvents {
         ),
       ),
     );
+
+    this.rawFrames$ = forAllPlayerIndices((index) => {
+      return this.stream$.pipe(
+        switchMap((stream) => stream.playerFrame$),
+        playerFrameFilter(index), //We only care about certain player frames
+      );
+    });
   }
 }
